371a9335ce948fd471df29a214f8eec9541784bc
leovern123 <umarcp31@gmail.com>
2025-11-05 11:09:10 +0700

    buat shape lingkaran

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -8,6 +8,18 @@ class MySplashScreen1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        body: Column(
+          children: [
+          Container(
+            width: 250,
+            height: 250,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.amber,
+              ),
+            ),
+          ],
+        )
       
     );
   }
